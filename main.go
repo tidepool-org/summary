40240b7576ad90f0c2ce43b55b343a250249176f
Derrick Burns <derrick@tidepool.org>
2020-08-14 20:00:10 -0700

    Refactor main

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/getkin/kin-openapi/openapi3filter"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/tidepool-org/summary/api"
+	"github.com/tidepool-org/summary/bgprovider"
+	"github.com/tidepool-org/summary/server"
+
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var (
+	// ServerTimeoutAmount is the amount of time before we time out the server
+	ServerTimeoutAmount = 20
+	_                   = openapi3filter.Options{}
+)
+
+//ServiceConfig the configuration for the summary service
+type ServiceConfig struct {
+	ServiceAuth string `envconfig:"TIDEPOOL_SUMMARY_SERVICE_SECRET" required:"true"`
+	Address     string `envconfig:"TIDEPOOL_SUMMARY_SERVICE_SERVER_ADDRESS" default:":8080"`
+}
+
+//NewServiceConfigFromEnv creates a service config
+func NewServiceConfigFromEnv() *ServiceConfig {
+	var config ServiceConfig
+	err := envconfig.Process("", &config)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return &config
+}
+
+//main is the main loop
+func main() {
+
+	config := NewServiceConfigFromEnv()
+
+	// Echo instance
+	e := echo.New()
+	e.Logger.Print("Starting Main Loop")
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		e.Logger.Fatal("Cound not get spec")
+	}
+
+	// Middleware
+	//authClient := AuthClient{store: dbstore}
+	//filterOptions := openapi3filter.Options{AuthenticationFunc: authClient.AuthenticationFunc}
+	//options := Options{Options: filterOptions}
+	options := api.Options{}
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(api.OapiRequestValidator(swagger, &options))
+
+	// Routes
+	e.GET("/", hello)
+
+	// Register Handler
+	api.RegisterHandlers(e, &server.SummaryServer{
+		Provider: &bgprovider.MockProvider{},
+	})
+
+	// Start server
+	e.Logger.Printf("Starting Server at: %s\n", config.Address)
+	go func() {
+		if err := e.Start(config.Address); err != nil {
+			e.Logger.Info("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 10 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(ServerTimeoutAmount)*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
---
+++ server/server.go
@@ -0,0 +1,73 @@
+package server
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/tidepool-org/summary/api"
+	"github.com/tidepool-org/summary/bgprovider"
+	"github.com/tidepool-org/summary/summarizer"
+)
+
+//SummaryServer provides summaries as a service
+type SummaryServer struct {
+	Provider bgprovider.BGProvider
+}
+
+var _ api.ServerInterface = &SummaryServer{} // confirms that interface is implemented
+
+// PostV1ClinicsCliniidSummary provides summaries for clinicians
+// (POST /v1/clinics/{clinicid}/summaries)
+func (c *SummaryServer) PostV1ClinicsCliniidSummary(ctx echo.Context, clinicid string) error {
+	var summaryRequest api.SummaryRequest
+
+	if err := ctx.Bind(&summaryRequest); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "error parsing parameters")
+	}
+
+	summarizer := summarizer.NewSummarizer(summaryRequest)
+	from, to := DateRange(summaryRequest)
+	ch := make(chan bgprovider.BG)
+
+	c.Provider.Get(ctx.Request().Context(), from, to, ch, false)
+
+	for {
+		select {
+		case <-ctx.Request().Context().Done():
+			return ctx.JSON(http.StatusRequestTimeout, nil)
+		case bg, ok := <-ch:
+			if !ok {
+				summary := summarizer.Summary()
+				return ctx.JSON(http.StatusOK, &summary)
+			}
+			summarizer.Process(bg)
+		}
+	}
+}
+
+//DateRange provide the times needed to produce the reports
+func DateRange(req api.SummaryRequest) (from, to time.Time) {
+	var numDays int
+	if req.Period.Length == "day" {
+		numDays = req.Period.NumPeriods
+	} else if req.Period.Length == "week" {
+		numDays = 7 * req.Period.NumPeriods
+	}
+	to = time.Now()
+	from = to.AddDate(0, 0, -numDays)
+	return
+}
+
+// PostV1UsersUseridSummary provides summaries for a given user
+// (POST /v1/users/{userid}/summary)
+func (c *SummaryServer) PostV1UsersUseridSummary(ctx echo.Context, userid string) error {
+	var summaryRequest api.SummaryRequest
+
+	if err := ctx.Bind(&summaryRequest); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "error parsing parameters")
+	}
+
+	summaryResponse := api.SummaryResponse{}
+	return ctx.JSON(http.StatusOK, &summaryResponse)
+}
