6b1adb28148e9bcf0c8a59e2a551e035eec3d33d
Derrick Burns <derrick@tidepool.org>
2020-08-26 13:28:46 -0700

    Added logging

--- dataprovider/shareprovider.go
+++ dataprovider/shareprovider.go
@@ -2,6 +2,7 @@ package dataprovider
 
 import (
 	"context"
+	"log"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -30,13 +31,20 @@ func NewMongoShareProvider(client *mongo.Client) *MongoShareProvider {
 //SharerIdsForUser returns the user ids of accounts that are shared with the given user
 func (b *MongoShareProvider) SharerIdsForUser(ctx context.Context, userID string) ([]string, error) {
 	perms := b.Client.Database("gatekeeper").Collection("perms")
-	sharerIds, err := perms.Distinct(ctx, "sharedId", bson.M{"userId": userID})
 
+	log.Printf("get distinct")
+	sharerIds, err := perms.Distinct(ctx, "sharedId", bson.M{"userId": userID})
+	if err != nil {
+		log.Fatalf("error getting distinct %v", err)
+	}
+	log.Printf("get distinct completed")
 	type Share struct {
 		ID string `bson:"sharerId"`
 	}
+	log.Printf("num ids %v", len(sharerIds))
 	ids := make([]string, len(sharerIds))
 	for i, id := range sharerIds {
+		log.Printf("i %d share %v", i, id)
 		ids[i] = id.(Share).ID
 	}
 	return ids, err
@@ -45,13 +53,18 @@ func (b *MongoShareProvider) SharerIdsForUser(ctx context.Context, userID string
 //SharerIdsForClinic returns the user ids of accounts that are shared with the given clinic
 func (b *MongoShareProvider) SharerIdsForClinic(ctx context.Context, clinicID string) ([]string, error) {
 	perms := b.Client.Database("clinic").Collection("clinicPatients")
+	log.Printf("get distinct")
 	sharerIds, err := perms.Distinct(ctx, "patientId", bson.M{"clinicId": clinicID})
-
+	if err != nil {
+		log.Fatalf("error getting distinct %v", err)
+	}
+	log.Printf("get distinct completed")
 	type Share struct {
 		ID string `bson:"patientId"`
 	}
 	ids := make([]string, len(sharerIds))
 	for i, id := range sharerIds {
+		log.Printf("i %d share %v", i, id)
 		ids[i] = id.(Share).ID
 	}
 	return ids, err
